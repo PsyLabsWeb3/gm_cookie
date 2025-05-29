ca702744e0306d4e21f792a82488a0048058353a
terratekmusic <terrateckmusic@gmail.com>
2025-05-29 14:45:16 -0600

    Spline Integration

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,8 @@
 import "./index.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
-import { AppSidebar } from "./components/app-sidebar";
+// import { AppSidebar } from "./components/app-sidebar";
+import Spline from '@splinetool/react-spline';
 import { TooltipProvider } from "./components/ui/tooltip";
 import { Toaster } from "./components/ui/toaster";
 import { BrowserRouter, Route, Routes } from "react-router";
@@ -31,11 +32,24 @@ function App() {
             >
                 <BrowserRouter>
                 <Navigation />
-                    <TooltipProvider delayDuration={0}>
+               
+                    <TooltipProvider delayDuration={0} 
+                                      
+                        >
                             <SidebarProvider>
                                 {/* <AppSidebar /> */}
                                 <SidebarInset>
-                                    <div className="flex flex-1 flex-col gap-4 size-full container">
+                                    <div className="flex flex-1 flex-col gap-4 size-full container"
+                                    style={{  position: "absolute",
+                                        top: 0,
+                                       paddingTop: "4rem",
+                                       height: "90%",
+                                        right: 0,
+                                        width: "60%",
+                                        backgroundColor: "rgba(0, 0, 0, 0)",
+                                        color: "#fff",
+                                        zIndex: 1000, }}
+                                    >
                                         <Routes>
                                             <Route path="/" element={<Home />} />
                                             <Route
@@ -48,10 +62,13 @@ function App() {
                                             />
                                         </Routes>
                                     </div>
+                                    <Spline  scene="https://prod.spline.design/JBN24EueGn3gq85M/scene.splinecode" />
                                 </SidebarInset>
                             </SidebarProvider>
                         <Toaster />
+                        
                     </TooltipProvider>
+                    
                 </BrowserRouter>
             </div>
         </QueryClientProvider>
